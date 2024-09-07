efaa18b12854d6ddb4c13b793407a49308564522
JesseZeph <jessezephyr@gmail.com>
2024-09-07 14:53:16 +0100

    feat: all sections done, custom animations with framer motion

---
+++ src/sections/ProductShowcase.tsx
@@ -0,0 +1,60 @@
+"use client";
+import productImage from "@/assets/product-image.png";
+import pyramidImage from "@/assets/pyramid.png";
+import Image from "next/image";
+import tubeImage from "@/assets/tube.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+  return (
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip"
+    >
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <h1 className="tag">Boost your productivity</h1>
+          </div>
+          <h2 className="section-title mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="section-description mt-5">
+            Effortlessly turn your ideas into fully functional, responsive Saas
+            website in just minutes with this template.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} alt="Product Image" className="mt-10" />
+          <motion.img
+            src={pyramidImage.src}
+            alt="Pyramid Image"
+            className="hidden md:block absolute -right-36 -top-32"
+            width={262}
+            height={262}
+            style={{
+              translateY,
+            }}
+          />
+          <motion.img
+            src={tubeImage.src}
+            alt="Tube Image"
+            className="hidden md:block absolute bottom-24 -left-36"
+            width={248}
+            height={248}
+            style={{
+              translateY,
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
